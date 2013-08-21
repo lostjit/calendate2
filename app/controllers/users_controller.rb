08fd8a87584c63599c87b7f72d87123056da243b
Phong Nguyen <lostjit@gmail.com>
2013-08-20 18:46:40 -0700

    added dynamic months

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,91 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(session[:user_id])
+
+    @time1 = Time.new
+    @year = @time1.year
+    @month = @time1.month
+    @day = 1
+
+    case @year
+      when 2013
+        @a = 2
+      when 2014
+        @a = 3
+      when 2015
+        @a = 4
+      when 2016
+       @a = 6
+      when 2017
+        @a = 7
+      when 2018
+        @a = 8
+      when 2019
+        @a = 9
+      when 2020
+        @a = 4
+      when 2021
+        @a = 5
+      when 2022
+        @a = 6
+      end
+
+      case @month
+        when 1
+          @b = 6
+          @numofdays = 31
+          @monthname = "January"
+        when 2
+          @b = 2
+          @numofdays = 28
+          @monthname = "February"
+        when 3
+          @b = 2
+          @numofdays = 31
+          @monthname = "March"
+        when 4
+          @b = 5
+          @numofdays = 30
+          @monthname = "April"
+        when 5
+          @b = 0
+          @numofdays = 31
+          @monthname = "May"
+        when 6
+          @b = 3
+          @numofdays = 30
+          @monthname = "June"
+        when 7
+          @b = 5
+          @numofdays = 31
+          @monthname = "July"
+        when 8
+          @b = 1
+          @numofdays = 31
+          @monthname = "August"
+        when 9
+          @b = 4
+          @numofdays = 30
+          @monthname = "September"
+        when 10
+          @b = 6
+          @numofdays = 31
+          @monthname = "October"
+        when 11
+          @b = 2
+          @numofdays = 30
+          @monthname = "November"
+        when 12
+          @b = 4
+          @numofdays = 31
+          @monthname = "December"
+       end
+
+       #variable to tell what day the 1st lands on for any given month/year (that we've listed)
+    @answer = (@day + @a + @b) % 7
+
   end
+
 
   private
   def user_params
