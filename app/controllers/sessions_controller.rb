9aa1c0165b894c369f934d7c071e90aba814e1fc
Phong Nguyen <lostjit@gmail.com>
2013-08-22 16:28:39 -0700

    meeeeh fixed double dig months

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,26 @@ class SessionsController < ApplicationController
 	  	else
 	  		sign_in user
 	  		@time1 = Time.new
-			redirect_to user_show_path(user, @time1)
+
+	  		@year = @time1.year
+		    @month = @time1.month
+		    @day = @time1.day
+
+		   if @month < 10
+       			 @realmonth = "-0" + @month.to_s
+     			 else
+       			@realmonth = "-" + @month.to_s
+      				end
+
+      		if @day < 10
+       			 @realday = "-0" + @day.to_s
+     			 else
+       			@realday = "-" + @day.to_s
+      				end
+
+      			@realdate = @year.to_s + @realmonth + @realday
+
+			redirect_to user_show_path(user, @realdate)
 	  	end
 	end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,32 @@ class UsersController < ApplicationController
     if @user.save
       sign_in @user
       @time1 = Time.new
-      redirect_to user_show_path(@user, @time1)
+
+      @year = @time1.year
+        @month = @time1.month
+        @day = @time1.day
+
+       if @month < 10
+             @realmonth = "-0" + @month.to_s
+           else
+            @realmonth = "-" + @month.to_s
+              end
+
+          if @day < 10
+             @realday = "-0" + @day.to_s
+           else
+            @realday = "-" + @day.to_s
+              end
+
+            @realdate = @year.to_s + @realmonth + @realday
+
+
+
+
+
+
+
+      redirect_to user_show_path(@user, @realdate)
     else
       @show = "register"
       render action: "index"
@@ -35,8 +60,11 @@ class UsersController < ApplicationController
     @month = @dateis.month
 
 
-
-     @minidate = @year.to_s + "-0" + @month.to_s + "%"
+      if @month < 10
+        @minidate = @year.to_s + "-0" + @month.to_s + "%"
+      else
+        @minidate = @year.to_s + "-" + @month.to_s + "%"
+      end
 
     @howmanyevents = Calendar.where("day_of_event LIKE ?", @minidate).where(user_id: current_user)
     @howmanyevents_length = @howmanyevents.length
